0461b9dbe9182be2b6e0511007e009b17e5ce7e4
Louis Aussedat <aussedat.louis@gmail.com>
2023-07-30 12:43:49 +0200

    commands/buy: add test to check call of load command

--- src/commands/__tests__/buy.test.ts
+++ src/commands/__tests__/buy.test.ts
@@ -2,14 +2,14 @@ import { mockNexo } from '../../api/__mocks__/nexo.mock';
 import * as NexoModule from '../../api/nexo';
 import * as LoggerModule from '../../utils/logger';
 import buy from '../buy';
+import * as LoadModule from '../load';
 
 jest.mock('../../api/nexo');
 jest.mock('../../utils/logger');
+jest.mock('../load');
 
 describe('Buy command', () => {
   const nexo = mockNexo();
-
-  const loadMock = jest.fn();
   jest.spyOn(LoggerModule, 'logOk').mockReturnValue(jest.fn() as any);
   jest.spyOn(NexoModule, 'Nexo').mockReturnValue(nexo);
 
@@ -20,5 +20,12 @@ describe('Buy command', () => {
   it('should call buy from nexo api', async () => {
     await buy({ pair: 'BTC/USD', ammount: 1000 }, {});
     expect(nexo.buy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call load command', async () => {
+    const loadMock = jest.spyOn(LoadModule, 'default').mockResolvedValue();
+    await buy({ pair: 'BTC/USD', ammount: 1000 }, {});
+    expect(loadMock).toHaveBeenCalledTimes(1);
+    expect(loadMock).toHaveBeenCalledWith({ id: 'id' }, {});
   });
 });
