04dc2dab1fa0113e15d9463e674586587a8cc105
Louis Aussedat <aussedat.louis@gmail.com>
2023-09-16 15:58:22 +0200

    rename NexoConfig to ExchangeConfig

--- src/types/config.ts
+++ src/types/config.ts
@@ -1,5 +1,5 @@
 export type Config = {
-  nexo: NexoConfig;
+  nexo: ExchangeConfig;
   ghostfolio: GhostfolioConfig;
 };
 
@@ -10,7 +10,7 @@ export type GhostfolioConfig = {
   accountId?: string;
 };
 
-export type NexoConfig = {
+export type ExchangeConfig = {
   key: string;
   secret: string;
 };
