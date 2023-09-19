85b00d481bfb0c978a5090fc8df606e0eb9cc8a4
Louis Aussedat <aussedat.louis@gmail.com>
2023-09-19 19:39:52 +0200

    services: add unit test for exchange, forex and crypto

---
+++ src/services/crypto/__tests__/coingecko.service.test.ts
@@ -0,0 +1,39 @@
+import 'reflect-metadata';
+
+import { Container } from 'inversify';
+
+import { ILogger } from '@app/logger/interface';
+import CoingeckoService from '@app/services/crypto/coingecko.service';
+import { TYPES } from '@app/types';
+
+describe('Service: Debug Logger', () => {
+  let container: Container;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    container = new Container();
+    const mockLoggerService: ILogger = {
+      debug: jest.fn(),
+      info: jest.fn(),
+      error: jest.fn(),
+    };
+    container
+      .bind<ILogger>(TYPES.LoggerService)
+      .toConstantValue(mockLoggerService);
+  });
+
+  it('should get the correct name', async () => {
+    const service = container.resolve(CoingeckoService);
+
+    expect(await service.getCryptoNameBySymbol('BTC')).toBe('bitcoin');
+    expect(await service.getCryptoNameBySymbol('ETH')).toBe('ethereum');
+    expect(await service.getCryptoNameBySymbol('XRP')).toBe('ripple');
+  });
+
+  it('should return undefined on error', async () => {
+    const service = container.resolve(CoingeckoService);
+
+    expect(await service.getCryptoNameBySymbol('aaaaa')).toEqual(undefined);
+  });
+});
---
+++ src/services/exchange/__tests__/nexo.service.test.ts
@@ -0,0 +1,136 @@
+import 'reflect-metadata';
+
+import { Container } from 'inversify';
+import * as NexoProModule from 'nexo-pro';
+
+import { IConfig } from '@app/config/config.service';
+import NexoService from '@app/services/exchange/nexo.service';
+import { TYPES } from '@app/types';
+import { Config } from '@app/types/config';
+
+jest.mock('nexo-pro');
+
+export const mockNexoPro = () => {
+  const getQuote = jest.fn();
+  const placeOrder = jest.fn();
+  const getOrderDetails = jest.fn();
+  const getOrders = jest.fn();
+
+  const client = jest.fn(() => ({
+    getQuote,
+    placeOrder,
+    getOrderDetails,
+    getOrders,
+  }));
+
+  return { client, getQuote, placeOrder, getOrderDetails, getOrders };
+};
+
+describe('Service: Debug Logger', () => {
+  let container: Container;
+  let nexoProMock: ReturnType<typeof mockNexoPro>;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    nexoProMock = mockNexoPro();
+    jest
+      .spyOn(NexoProModule, 'default')
+      .mockImplementation(nexoProMock.client as any);
+
+    container = new Container();
+    container.bind<IConfig>(TYPES.ConfigService).toConstantValue({
+      config: {
+        nexo: { key: 'key', secret: 'secret' },
+      } as Config,
+      saveConfig: jest.fn(),
+    });
+  });
+
+  it('should initialize client with correct params', async () => {
+    container.resolve(NexoService);
+
+    expect(nexoProMock.client).toBeCalledWith({
+      api_key: 'key',
+      api_secret: 'secret',
+    });
+  });
+
+  it('should get a price quote correctly', async () => {
+    nexoProMock.getQuote.mockResolvedValue({
+      price: 42,
+    });
+
+    const service = container.resolve(NexoService);
+
+    expect(
+      await service.getQuote({ pair: 'BTC/USD', amount: 10 })
+    ).toStrictEqual({ price: 42 });
+  });
+
+  it('should return undefined on quote error', async () => {
+    nexoProMock.getQuote.mockRejectedValue(new Error());
+
+    const service = container.resolve(NexoService);
+
+    expect(
+      await service.getQuote({ pair: 'BTC/USD', amount: 10 })
+    ).toStrictEqual(undefined);
+  });
+
+  it('should get all orders correctly when no order', async () => {
+    nexoProMock.getOrders.mockResolvedValue({ orders: [] });
+
+    const service = container.resolve(NexoService);
+
+    expect(await service.getOrders({ pair: 'BTC/USD' })).toStrictEqual({
+      orders: [],
+    });
+  });
+
+  it('should get all orders correctly when multiple orders', async () => {
+    nexoProMock.getOrders.mockResolvedValue({
+      orders: [{ id: 'id1' }, { id: 'id2' }],
+    });
+
+    const service = container.resolve(NexoService);
+
+    expect(await service.getOrders({ pair: 'BTC/USD' })).toStrictEqual({
+      orders: [{ id: 'id1' }, { id: 'id2' }],
+    });
+  });
+
+  it('should return undefined on get orders error', async () => {
+    nexoProMock.getOrders.mockRejectedValue(new Error());
+
+    const service = container.resolve(NexoService);
+
+    expect(await service.getOrders({ pair: 'BTC/USD' })).toStrictEqual(
+      undefined
+    );
+  });
+
+  it('should get all orders correctly when multiple orders', async () => {
+    nexoProMock.placeOrder.mockResolvedValue({
+      orderId: 'id',
+    });
+
+    const service = container.resolve(NexoService);
+
+    expect(
+      await service.placeOrder({ pair: 'BTC/USD', quantity: 10 })
+    ).toStrictEqual({
+      id: 'id',
+    });
+  });
+
+  it('should return undefined on place order error', async () => {
+    nexoProMock.placeOrder.mockRejectedValue(new Error());
+
+    const service = container.resolve(NexoService);
+
+    expect(
+      await service.placeOrder({ pair: 'BTC/USD', quantity: 10 })
+    ).toStrictEqual(undefined);
+  });
+});
